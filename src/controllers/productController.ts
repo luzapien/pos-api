03f1024187614a72e88b0440dc593ec5aa0a254c
María Rodríguez <lu.rzapien.92@gmail.com>
2025-02-01 14:05:20 -0600

    feat: get by id, update and delete endpoits

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -4,7 +4,6 @@ export const getAllProducts = async (_req, res) => {
   try {
     const products = await prisma.product.findMany()
     res.json(products)
-    console.log('PRODUCTOS', products)
   } catch (error) {
     console.log(error)
     res.status(500).json({ error: 'Failed to fetch products' })
@@ -22,5 +21,60 @@ export const createProduct = async (req, res) => {
   } catch (error) {
     console.log(error)
     res.status(500).json({ error: 'Failed to create product' })
+  }
+}
+
+export const getProductById = async (req, res) => {
+  try {
+    const productId = Number.parseInt(req.params.id, 10)
+
+    if (Number.isNaN(productId)) {
+      return res.status(400).json({ error: 'Invalid product ID' })
+    }
+    const productById = await prisma.product.findUnique({
+      where: {
+        id: productId,
+      },
+    })
+    if (!productById) {
+      return res.status(404).json({ error: 'Product not found' })
+    }
+    return res.status(200).json(productById)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: 'Failed to fetch product by Id' })
+  }
+}
+
+export const updateProduct = async (req, res) => {
+  try {
+    const productId = Number.parseInt(req.params.id, 10)
+    const updateData = req.body
+    const updatedProduct = await prisma.product.update({
+      where: {
+        id: productId,
+      },
+      data: updateData,
+    })
+    console.log('UPDATED', updatedProduct)
+    return res.status(200).json(updatedProduct)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: 'Failed to update product' })
+  }
+}
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const productId = Number.parseInt(req.params.id, 10)
+    const deleteProduct = await prisma.product.delete({
+      where: {
+        id: productId,
+      },
+    })
+    return res.status(200).json(` ${deleteProduct.name} deleted`)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: 'Failed to delete product' })
   }
 }
--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,9 +1,18 @@
-import { createProduct, getAllProducts } from '@/controllers/productController.js'
+import {
+  createProduct,
+  deleteProduct,
+  getAllProducts,
+  getProductById,
+  updateProduct,
+} from '@/controllers/productController.js'
 import { Router } from 'express'
 
 const productRouter = Router()
 
 productRouter.get('/', getAllProducts)
 productRouter.post('/', createProduct)
+productRouter.get('/:id', getProductById)
+productRouter.put('/:id', updateProduct)
+productRouter.delete('/:id', deleteProduct)
 
 export { productRouter }
