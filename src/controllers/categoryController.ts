e0af1ee3745be04a62b6b3b806a26503b2f4e1d2
María Rodríguez <lu.rzapien.92@gmail.com>
2025-02-01 15:53:52 -0600

    feature: categories endpoints

---
+++ src/controllers/categoryController.ts
@@ -0,0 +1,76 @@
+import { prisma } from '@/db/prisma.js'
+import type { Request, RequestHandler, Response } from 'express'
+
+export const getAllCategories: RequestHandler = async (_req, res) => {
+  try {
+    const categories = await prisma.category.findMany()
+    res.status(200).json(categories)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: `Failed to get all categories: ${error}` })
+  }
+}
+
+export const createCategory: RequestHandler = async (req, res) => {
+  try {
+    const { name } = req.body
+    const newCategory = await prisma.category.create({
+      data: { name },
+    })
+    res.status(200).json(newCategory)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: `Failed to create category ${error}` })
+  }
+}
+
+export const getCategoryById: RequestHandler = async (req: Request, res: Response) => {
+  try {
+    const categoryById = await prisma.category.findUnique({
+      where: {
+        id: req.params.id,
+      },
+    })
+    if (!categoryById) {
+      res.status(404).json({ error: 'Product not found' })
+      return
+    }
+    res.status(200).json(categoryById)
+    return
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: `Failed to get category by id ${error}` })
+  }
+}
+
+export const updateCategory: RequestHandler = async (req: Request, res: Response) => {
+  try {
+    const updateData = req.body
+    const updatedCategory = await prisma.category.update({
+      where: {
+        id: req.params.id,
+      },
+      data: updateData,
+    })
+    res.status(200).json(updatedCategory)
+    return
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: `Failed to update category: ${error}` })
+  }
+}
+
+export const deleteCategory: RequestHandler = async (req: Request, res: Response) => {
+  try {
+    const categoryDeleted = await prisma.category.delete({
+      where: {
+        id: req.params.id,
+      },
+    })
+    res.status(200).json(`${categoryDeleted.name} deleted`)
+    return
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: `Failed to dete category ${error}` })
+  }
+}
